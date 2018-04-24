73512ccd8d267f0f41d27669f9abac45fc826d12
K4masz <k4masz@gmail.com>
2018-04-24 09:26:14 +0200

    Added:  - .gitignore Modified:  - using webdriver instead of system property

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -3,9 +3,14 @@ package tests;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public abstract class BaseTest {
     public WebDriver driver;
@@ -13,8 +18,12 @@ public abstract class BaseTest {
 
     @BeforeClass
     public void setup() {
-        System.setProperty("webdriver.chrome.driver", "E:\\QA\\drivers\\chromedriver.exe");
-        driver = new ChromeDriver();
+        DesiredCapabilities capability = DesiredCapabilities.chrome();
+        try {
+            driver = new RemoteWebDriver(new URL("http://127.0.0.1:9515"),capability);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
         driver.get("https://rekrutacjaqa2.xsolve.software");
         wait = new WebDriverWait(driver, 10);
         driver.manage().window().maximize();
